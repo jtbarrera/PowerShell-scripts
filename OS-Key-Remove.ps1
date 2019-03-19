11eab12b5074fd5c315d3cc08f47c45477022c80
Javier Barrera <jtbarrera@gmail.com>
2019-03-19 13:53:41 -0500

    update to write to text file

--- OS-Key-Remove.ps1
+++ OS-Key-Remove.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Function: OS-Key-Remove <parameter> 
 Purpose:
 The funtion will by default query local machine for status OS key. It can optionally remove the OS key from the OS and the registry hive.
@@ -24,22 +24,28 @@ Function OS-Key-Remove {
 
     Process 
     { 
-        try{ 
+        try{
+            $retryCount = 3
+            $outFilePath = "c:\temp"
+            $outFile = "scriptresult.txt"
+            
+            #create result file
+            New-item -path $outFilePath -name $outFile -ItemType "file" 
+            
             #Removing the KEY 
-            $retryCount = 3 
             while ($retryCount -gt 0) 
             { 
                 if($para -eq "remove") {
-                    Write-Output "Removing License Key..." 
+                    Write-Output "Removing License Key..." | out-file $outFilePath\$outFile -Append
                     #trying to Uninstall key.... 
                     #cscript $env:SystemRoot\system32\slmgr.vbs /upk
-                    Write-Output "Removing License Key from Registry..."
+                    Write-Output "Removing License Key from Registry..." | out-file $outFilePath\$outFile -Append
                     #trying to remove key from registry
                     #cscript $env:SystemRoot\system32\slmgr.vbs /cpky
                 #verification process
                 }
                 if($para -eq "remove" -Or $para -eq "status") {
-                                Write-Output "Verifying Activation Status..." 
+                                Write-Output "Verifying Activation Status..." | out-file $outFilePath\$outFile -Append
                                 #checking the activation status..... 
                                 $slmgrResult = cscript $env:SystemRoot\system32\slmgr.vbs /dli 
                                 [string]$licenseStatus = ($slmgrResult | select-string -pattern "^License Status:") 
@@ -47,19 +53,22 @@ Function OS-Key-Remove {
  
                                 if ( $licenseStatus -match "Licensed")  
                                 { 
-                                    Write-Host "Windows Licensed" -ForegroundColor Green 
+                                    Write-Host "Windows Licensed" -ForegroundColor Green
+                                    Write-Output "Windows Licensed" | out-file $outFilePath\$outFile -Append
                                     $retryCount = 0 
                                 } 
                  
                                 else 
                                 { 
                                     Write-Host "Windows NOT Licensed." -ForegroundColor Red 
+                                    Write-Output "Windows NOT Licensed." | out-file $outFilePath\$outFile -Append
                                     $retryCount = $retryCount - 1 
                                 } 
  
                                 if ( $retryCount -gt 0 )  
                                 { 
                                     Write-Host "Retrying Process. Will try $retryCount more time(s)" -ForegroundColor Yellow 
+                                    Write-Output "Retrying Process. Will try $retryCount more time(s)" | out-file $outFilePath\$outFile -Append
                                 }
                 }
             }
@@ -75,3 +84,4 @@ Function OS-Key-Remove {
     {     
     } 
 }
+OS-Key-Remove 
